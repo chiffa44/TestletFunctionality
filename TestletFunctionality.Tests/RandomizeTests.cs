205d517ab917653333e8b1cd2362e9816be889a9
Elena Selivanova <chiffa44@gmail.com>
2022-09-03 01:08:14 +0300

    [Test]: 2 first pretests

--- TestletFunctionality.Tests/RandomizeTests.cs
+++ TestletFunctionality.Tests/RandomizeTests.cs
@@ -1,13 +1,36 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestletFunctionality.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class RandomizeTests
     {
+        private List<Test> tests = new List<Test>()
+            {new Test("1", TestTypeEnum.Pretest),
+             new Test("2", TestTypeEnum.Operational),
+             new Test("3", TestTypeEnum.Operational),
+             new Test("4", TestTypeEnum.Operational),
+             new Test("5", TestTypeEnum.Pretest),
+             new Test("6", TestTypeEnum.Operational),
+             new Test("7", TestTypeEnum.Pretest),
+             new Test("8", TestTypeEnum.Operational),
+             new Test("9", TestTypeEnum.Pretest),
+             new Test("10", TestTypeEnum.Operational),
+           };
+
         [TestMethod]
-        public void TestMethod1()
+        public void TwoFirstPretests()
         {
+            //Arrange
+            Testlet testlet = new Testlet("testId", tests);
+            //Act
+            var actualResult = testlet.Randomize();
+            //Assert
+            Assert.IsTrue(actualResult[0].Type == TestTypeEnum.Pretest,
+                string.Format("First test should be pretest"));
+            Assert.IsTrue(actualResult[0].Type == TestTypeEnum.Pretest && actualResult[1].Type == TestTypeEnum.Pretest,
+                string.Format("Second test should be pretest"));
         }
     }
 }
--- TestletFunctionality/Testlet.cs
+++ TestletFunctionality/Testlet.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TestletFunctionality
 {
-    public class Class1
+    public class Testlet
     {
+        public string TestletId;
+        private List<Test> Items;
+        public Testlet(string testletId, List<Test> items)
+        {
+            TestletId = testletId;
+            Items = items;
+        }
+        public List<Test> Randomize()
+        {
+            //Items private collection has 6 Operational and 4 Pretest Items.
+            //Randomize the order of these items as per the requirement(with TDD)
+            //The assignment will be reviewed on the basis of – Tests written first, Correct
+            //logic, Well structured &clean readable code.
+            throw new NotImplementedException();
+        }
+    }
+    public class Test
+    {
+        public string Id;
+        public TestTypeEnum Type;
+        public Test(string id, TestTypeEnum type)
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+    public enum TestTypeEnum
+    {
+        Pretest = 0,
+        Operational = 1
     }
 }
