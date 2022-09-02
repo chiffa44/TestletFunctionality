4c9eb265cbd763656d1998e380fe90dcb7895da7
Elena Selivanova <chiffa44@gmail.com>
2022-09-03 01:20:49 +0300

    Get 2 first pretests

--- TestletFunctionality/Testlet.cs
+++ TestletFunctionality/Testlet.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TestletFunctionality
 {
     public class Testlet
     {
-        public string TestletId;
+        public string Id;
         private List<Test> Items;
         public Testlet(string testletId, List<Test> items)
         {
-            TestletId = testletId;
+            Id = testletId;
             Items = items;
         }
         public List<Test> Randomize()
         {
-            //Items private collection has 6 Operational and 4 Pretest Items.
-            //Randomize the order of these items as per the requirement(with TDD)
-            //The assignment will be reviewed on the basis of – Tests written first, Correct
-            //logic, Well structured &clean readable code.
-            throw new NotImplementedException();
+           var pretests = Items.Where(t => t.Type == TestTypeEnum.Pretest).Take(2);
+           var other = Items.Except(pretests);
+           return pretests.Concat(other).ToList();
         }
     }
     public class Test
